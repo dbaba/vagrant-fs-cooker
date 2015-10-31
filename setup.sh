837a71af4c81107ce6f0344e62433fc139c8d6d6
Daisuke Baba <baba.daisuke@gmail.com>
2015-10-31 20:19:45 +0900

    Add Vagrantfile and its provisioning script

---
+++ setup.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+AUTO_CONF_SCRIPT_PATH="${AUTO_CONF_SCRIPT_PATH:-/vagrant/auto_conf.sh}"
+
+function err {
+ echo -e "\033[91m$1\e[0m"
+}
+
+function info {
+ echo -e "\033[92m$1\e[0m"
+}
+
+for f in mount_fs umount_fs
+do
+  rm -f /home/vagrant/${f}.sh
+  ln -sn /vagrant/${f}.sh /home/vagrant/${f}.sh
+done
+
+if [ -f ${AUTO_CONF_SCRIPT_PATH} ]; then
+  rm -f "${AUTO_CONF_SCRIPT_PATH}.exit"
+  info "::: Starting the automatic configuration script.... :::"
+  . /home/vagrant/mount_fs.sh
+  RET=$?
+  if [ "${RET}" != "0" ]; then
+    err "Failed to set up"
+    exit 1
+  fi
+  if [ ! -x ${AUTO_CONF_SCRIPT_PATH} ]; then
+    chmod +x ${AUTO_CONF_SCRIPT_PATH}
+  fi
+  . ${AUTO_CONF_SCRIPT_PATH}
+  RET=$?
+  . /home/vagrant/umount_fs.sh
+  info "::: The automatic configuration script is terminated :::"
+  if [ "${RET}" == "0" ]; then
+    echo "0" > "${AUTO_CONF_SCRIPT_PATH}.exit"
+    info "HALTING..."
+    sudo shutdown -h now
+    exit 1
+  fi
+fi
